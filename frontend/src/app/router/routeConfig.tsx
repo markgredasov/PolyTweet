3b603f66fd844fc2137c92117dc34bf96eef3851
hellstaff98 <aleksey_2907@mail.ru>
2026-04-11 20:16:52 +0300

    Prettier config

--- frontend/src/app/router/routeConfig.tsx
+++ frontend/src/app/router/routeConfig.tsx
@@ -1,24 +1,31 @@
-import { RouteProps } from "react-router-dom";
-import AuthenticationPageAsync from "@pages/AuthenticationPage/AuthenticationPage.async";
-import FeedPageAsync from "@pages/FeedPage/FeedPage.async";
+import { RouteProps } from 'react-router-dom';
+import AuthenticationPageAsync from '@pages/AuthenticationPage/AuthenticationPage.async';
+import FeedPageAsync from '@pages/FeedPage/FeedPage.async';
+import ProfilePageAsync from '@pages/ProfilePage/ProfilePage.async';
 
 export enum AppRoutes {
-    AUTH = 'auth',
-    FEED = 'feed',
+  AUTH = 'auth',
+  FEED = 'feed',
+  PROFILE = 'profile',
 }
 
 export const RoutePath: Record<AppRoutes, string> = {
-    [AppRoutes.AUTH]: '/auth',
-    [AppRoutes.FEED]: '/feed',
-}
+  [AppRoutes.AUTH]: '/auth',
+  [AppRoutes.FEED]: '/feed',
+  [AppRoutes.PROFILE]: '/profile',
+};
 
 export const routeConfig: Record<AppRoutes, RouteProps> = {
-    [AppRoutes.AUTH]: {
-        path: RoutePath.auth,
-        element: <AuthenticationPageAsync />
-    },
-    [AppRoutes.FEED]: {
-        path: RoutePath.feed,
-        element: <FeedPageAsync />
-    }
-}
+  [AppRoutes.AUTH]: {
+    path: RoutePath.auth,
+    element: <AuthenticationPageAsync />,
+  },
+  [AppRoutes.FEED]: {
+    path: RoutePath.feed,
+    element: <FeedPageAsync />,
+  },
+  [AppRoutes.PROFILE]: {
+    path: RoutePath.feed,
+    element: <ProfilePageAsync />,
+  },
+};
